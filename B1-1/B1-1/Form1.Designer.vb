fac3e12f99c8945a725f5102df054cb09417a4cc
Atticz22 <attilacz22@gmail.com>
2022-03-12 18:17:31 +0200

    Second degree ecuation calculator

---
+++ B1-1/B1-1/Form1.Designer.vb
@@ -0,0 +1,200 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmEcuatii
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtbA = New System.Windows.Forms.TextBox()
+        Me.txtbB = New System.Windows.Forms.TextBox()
+        Me.txtbC = New System.Windows.Forms.TextBox()
+        Me.txtbX1 = New System.Windows.Forms.TextBox()
+        Me.txtbX2 = New System.Windows.Forms.TextBox()
+        Me.btnCALCULEAZA = New System.Windows.Forms.Button()
+        Me.btnIESIRE = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(24, 36)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(20, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "a:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(24, 94)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(20, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "b:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(24, 155)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(20, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "c:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(249, 21)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(166, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Radacinile ecuatiei sunt:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.Location = New System.Drawing.Point(249, 58)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(27, 17)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "x1:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label6.Location = New System.Drawing.Point(249, 94)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(27, 17)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "x2:"
+        '
+        'txtbA
+        '
+        Me.txtbA.Location = New System.Drawing.Point(61, 33)
+        Me.txtbA.Name = "txtbA"
+        Me.txtbA.Size = New System.Drawing.Size(119, 25)
+        Me.txtbA.TabIndex = 6
+        '
+        'txtbB
+        '
+        Me.txtbB.Location = New System.Drawing.Point(61, 91)
+        Me.txtbB.Name = "txtbB"
+        Me.txtbB.Size = New System.Drawing.Size(119, 25)
+        Me.txtbB.TabIndex = 7
+        '
+        'txtbC
+        '
+        Me.txtbC.Location = New System.Drawing.Point(61, 152)
+        Me.txtbC.Name = "txtbC"
+        Me.txtbC.Size = New System.Drawing.Size(119, 25)
+        Me.txtbC.TabIndex = 8
+        '
+        'txtbX1
+        '
+        Me.txtbX1.Location = New System.Drawing.Point(296, 55)
+        Me.txtbX1.Name = "txtbX1"
+        Me.txtbX1.Size = New System.Drawing.Size(119, 25)
+        Me.txtbX1.TabIndex = 9
+        '
+        'txtbX2
+        '
+        Me.txtbX2.Location = New System.Drawing.Point(296, 91)
+        Me.txtbX2.Name = "txtbX2"
+        Me.txtbX2.Size = New System.Drawing.Size(119, 25)
+        Me.txtbX2.TabIndex = 10
+        '
+        'btnCALCULEAZA
+        '
+        Me.btnCALCULEAZA.Location = New System.Drawing.Point(226, 177)
+        Me.btnCALCULEAZA.Name = "btnCALCULEAZA"
+        Me.btnCALCULEAZA.Size = New System.Drawing.Size(122, 31)
+        Me.btnCALCULEAZA.TabIndex = 11
+        Me.btnCALCULEAZA.Text = "&Calculeaza"
+        Me.btnCALCULEAZA.UseVisualStyleBackColor = True
+        '
+        'btnIESIRE
+        '
+        Me.btnIESIRE.Location = New System.Drawing.Point(365, 177)
+        Me.btnIESIRE.Name = "btnIESIRE"
+        Me.btnIESIRE.Size = New System.Drawing.Size(122, 31)
+        Me.btnIESIRE.TabIndex = 12
+        Me.btnIESIRE.Text = "I&esire"
+        Me.btnIESIRE.UseVisualStyleBackColor = True
+        '
+        'frmEcuatii
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(518, 230)
+        Me.Controls.Add(Me.btnIESIRE)
+        Me.Controls.Add(Me.btnCALCULEAZA)
+        Me.Controls.Add(Me.txtbX2)
+        Me.Controls.Add(Me.txtbX1)
+        Me.Controls.Add(Me.txtbC)
+        Me.Controls.Add(Me.txtbB)
+        Me.Controls.Add(Me.txtbA)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "frmEcuatii"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Ecuatia de gradul II"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtbA As TextBox
+    Friend WithEvents txtbB As TextBox
+    Friend WithEvents txtbC As TextBox
+    Friend WithEvents txtbX1 As TextBox
+    Friend WithEvents txtbX2 As TextBox
+    Friend WithEvents btnCALCULEAZA As Button
+    Friend WithEvents btnIESIRE As Button
+End Class
---
+++ B1-1/B1-1/Form1.vb
@@ -0,0 +1,84 @@
+﻿Imports System.ComponentModel
+
+Public Class frmEcuatii
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtbA.TextChanged
+        If Not IsNumeric(txtbA.Text) Then
+            MessageBox.Show("Trebuie introdusa valori corecte", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtbA.SelectAll()
+            txtbA.Focus()
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnIESIRE.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnCALCULEAZA.Click
+        Dim a, b, c, D, x1, x2 As Single
+
+
+        a = txtbA.Text
+        b = txtbB.Text
+        c = txtbC.Text
+
+        D = b ^ 2 - 4 * a * c
+
+        If D < 0 Then
+            MessageBox.Show("Eroare: Solutii complexe!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+        Else
+            x1 = (-b + Math.Sqrt(D)) / (2 * a)
+            x2 = (-b - Math.Sqrt(D)) / (2 * a)
+            txtbX1.Text = Math.Round(x1, 2)
+            txtbX2.Text = Math.Round(x2, 2)
+
+        End If
+
+        If D = 0 Then
+            MessageBox.Show("Ecuatia are o singura solutie!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+
+    End Sub
+
+    Private Sub txtbB_TextChanged(sender As Object, e As EventArgs) Handles txtbB.TextChanged
+        If Not IsNumeric(txtbB.Text) Then
+            MessageBox.Show("Trebuie introdusa valori corecte", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtbB.SelectAll()
+            txtbB.Focus()
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub txtbC_TextChanged(sender As Object, e As EventArgs) Handles txtbC.TextChanged
+        If Not IsNumeric(txtbC.Text) Then
+            MessageBox.Show("Trebuie introdusa valori corecte", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtbC.SelectAll()
+            txtbC.Focus()
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub txtbB_Validating(sender As Object, e As CancelEventArgs) Handles txtbB.Validating
+        If Not IsNumeric(txtbB.Text) Then
+            e.Cancel = True
+
+        End If
+    End Sub
+End Class
